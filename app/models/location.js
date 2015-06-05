9964d66d5b8861c15ae327e7a7869906a486f9c6
mikegrassotti <mgrassotti@gmail.com>
2015-06-05 11:12:37 -0400

    make hack-map show allWorkstations

--- app/models/location.js
+++ app/models/location.js
@@ -2,5 +2,15 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  rooms: DS.hasMany({async: true})
+  rooms: DS.hasMany({async: true}),
+  allWorkstations: function() {
+    var array = []
+    this.get('rooms').map(function(room) {
+      room.get('workstations').forEach(function(workstation) {
+        console.log('workstation', workstation.get('name'));
+        array.pushObject(workstation);
+      });
+    })
+    return array;
+  }.property('rooms.workstations.@each')
 });
